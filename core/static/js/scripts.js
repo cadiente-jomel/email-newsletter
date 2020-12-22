23e54ff02e461e418330b2ca4336e1b389409283
cadiente-jomel <cadientejomel0@gmail.com>
2020-12-22 14:11:39 +0800

    server: model changes

--- core/static/js/scripts.js
+++ core/static/js/scripts.js
@@ -1,24 +1,94 @@
-$(function(){
-    $('.one form .btn').on('click',function(){ 
-       $(this).parents('.one').animate({
-            top : '-500px'
-          },500);
-      
-                                            $(this).parents('.one').siblings('.two').
-       animate({
-            top : '0px'
-          },500);
-      return false;
+$(function () {
+    $('.one form .btn').on('click', function (e) {
+        let validEmailRegEx = /^[A-Z0-9_'%=+!`#~$*?^{}&|-]+([\.][A-Z0-9_'%=+!`#~$*?^{}&|-]+)*@[A-Z0-9-]+(\.[A-Z0-9-]+)+$/i
+        const email = document.querySelector('.email').value;
+        let isEmailValid = validEmailRegEx.test(email);
+        checkEmail(isEmailValid, email)
+        e.preventDefault()
+        
+       
+        return false;
     });
-  
-  $('.two .close').on('click',function(){
-    $(this).parent().animate({
-     top : '-500px'
-    },500);
-    
-    $(this).parent().siblings('.one').animate({
-     top : '0px'
-    },500);
-    return false;
-   });
-  });
+
+    $('.two .close').on('click', function () {
+        $(this).parent().animate(
+            {
+                top: '-500px',
+            },
+            500
+        );
+
+        $(this).parent().siblings('.one').animate(
+            {
+                top: '0px',
+            },
+            500
+        );
+        return false;
+    });
+
+    const checkEmail = (flag, email) => {
+      if (flag) {
+        let message = 'thank you for subscribing! please check your email'
+        displayMessage(message);
+        data = {
+          email: email
+        }
+        fetch('/send/welcome/', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrftoken
+          },
+          body: JSON.stringify(data)
+        })
+      }
+
+      else {
+        let message = 'You have entered invalid email please try again!'
+        displayMessage(message)
+
+    }
+    }
+    const displayMessage = (textMessage) => {
+      let message = document.querySelector('.h3__text')
+      message.textContent = textMessage
+      $('.one form .btn').parents('.one').animate(
+        {
+            top: '-500px',
+        },
+        500
+      );
+
+      $('.one form .btn').parents('.one').siblings('.two').animate(
+          {
+              top: '0px',
+          },
+          500
+      );
+
+    }
+});
+
+function getCookie(name) {
+  let cookieValue = null;
+  if (document.cookie && document.cookie !== '') {
+      const cookies = document.cookie.split(';');
+      for (let i = 0; i < cookies.length; i++) {
+          const cookie = cookies[i].trim();
+          // Does this cookie string begin with the name we want?
+          if (cookie.substring(0, name.length + 1) === (name + '=')) {
+              cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+              break;
+          }
+      }
+  }
+  return cookieValue;
+}
+const csrftoken = getCookie('csrftoken');
+
+    // const form = document.querySelector('.form')
+
+    // form.addEventListener('submit', e => {
+
+    // })
--- core/views.py
+++ core/views.py
@@ -1,31 +1,45 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Email
 from django.core.mail import send_mail, EmailMultiAlternatives
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 from django.conf import settings
+from json import loads
 # Create your views here.
 
-def indexPage(request):
-
-    subject = 'Thank you for registering to our site'
-    message = 'it means the world to us'
-    email_from = settings.EMAIL_HOST_USER
-    recipient_list = ['cadientejomel4@gmail.com']
-    try:
-        html_content = render_to_string('core/templates.html')
-        text_content = strip_tags(html_content)
-        email = EmailMultiAlternatives(
-            subject,
-            message,
-            email_from,
-            recipient_list,
-        )
-        email.attach_alternative(html_content, 'text/html')
-        email.send()
+def indexPage(request):    
+    if request.method == 'POST':
+        # try:
+        subject = 'You are now subscribed to my weekly newspaper'
+        message = 'check out what\'s hot in tech right now'
+        email_from = settings.EMAIL_HOST_USER
+        recipient_list = list()
+        payload = loads(request.body.decode('utf-8'))
+        email = payload['email']
+        is_exists = Email.objects.filter(email=email).exists()
+        if not is_exists:
+            new_subscriber = Email.objects.create(email=email)
+            new_subscriber.save()
+            recipient_list.append(new_subscriber)
+            html_content = render_to_string('core/templates.html')
+            text_content = strip_tags(html_content)
+            email = EmailMultiAlternatives(
+                subject,
+                message,
+                email_from,
+                recipient_list,
+            )
+            email.attach_alternative(html_content, 'text/html')
+            email.send()
         return HttpResponse('email sent')
-    except:
-        return HttpResponse('there\'s an error sending the message')
+        # except:
+        #     return HttpResponse('there\'s an error sending the message')
+
+    
+
+
+  
     # try:
     #     send_mail(subject, message, email_from, recipient_list, fail_silently=False,)
     #     return HttpResponse('email sent')
